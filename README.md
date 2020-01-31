46fcf29ec9026692953d11569b43da4d95f4c85c
Mike Warren <mikeywarren89@gmail.com>
2020-01-31 16:43:57 +0900

    first commit, basic structure working

---
+++ README.md
@@ -0,0 +1,18 @@
+Craft Beer Book Project
+
+Overview:
+Build a basic log book of craft beers I have tried with info
+about the beers along with images which will be scraped online.
+
+Basic functionality:
+- User can add beers by name (searched on beeradvocate.com and scraped)
+- Manual entry for beers not on BA
+- Scrape will pull in facts and image
+- User can list all beers in the book
+- Will all be saved to SQL DB
+- Will be a basic webapp in Sinatra
+
+Tech:
+- Ruby
+- Sinatra for web app
+- SQL.lite for DB
---
+++ lib/app.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'sqlite3'
+
+DB = SQLite3::Database.new(File.join(File.dirname(__FILE__), 'db/craft-beer-book'))
+DB.results_as_hash = true
+get '/' do
+  @beers = DB.execute('SELECT * FROM beers')
+  erb :home
+end
+
+# -- TODO --
+# Implement a way to manually add beers
+# Nav bar in layout? - click for options, add etc
+# Create a Scraper class to get the data from BA (models?)
+# Research how to scrape images, save them and then reference them in the DB
