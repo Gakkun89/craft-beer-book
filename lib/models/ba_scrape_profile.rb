a81b1b5412600b87545e91e259f3427b65930972
Mike Warren <mikeywarren89@gmail.com>
2020-02-09 14:32:33 +0900

    scrape of profile fully working and tested

--- lib/models/ba_scrape_profile.rb
+++ lib/models/ba_scrape_profile.rb
@@ -3,12 +3,29 @@ require "open-uri"
 
 class BAScrapeProfile
   def initialize(url)
-    @url = "beertalker.html"
+    @url = "https://www.beeradvocate.com/beer/shelf-talker/?b=#{url}"
     @doc = Nokogiri::HTML(open(@url))
+    @rawbeer = []
+    @beer = {
+      name: '',
+      brewery: '',
+      abv: 0.0,
+      style: '',
+      bascore: ''
+    }
   end
 
-# TODO scrape all dd from the beer talker, then return img url from profile send all back as hash
+  def scrape
+    @doc.search('tr').each do |tr|
+      @rawbeer << tr.text.strip
+    end
+    cleaned_text = @rawbeer[2].split("\n")
+    @beer = {
+      name: cleaned_text[0],
+      brewery: cleaned_text[9],
+      abv: cleaned_text[7].gsub('%', '').to_f,
+      style: cleaned_text[5],
+      bascore: cleaned_text[3].delete("^0-9")
+    }
+  end
 end
-BAScrapeProfile.new("lol")
-# consider shelf talker for easy scrape: https://www.beeradvocate.com/beer/shelf-talker/?b=97563
-# get img from main page
