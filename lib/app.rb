7c12b3458c75655907ce988c7e432aab965b9368
Mike Warren <mikeywarren89@gmail.com>
2020-02-09 16:24:51 +0900

    ui path for adding a beer from BA works, now need to write to DB

--- lib/app.rb
+++ lib/app.rb
@@ -5,6 +5,8 @@ require 'sinatra/reloader' if development?
 require 'sqlite3'
 require 'sinatra/activerecord'
 require_relative 'models/beer'
+require_relative 'models/ba_scrape_results'
+require_relative 'models/ba_scrape_profile'
 
 DB = SQLite3::Database.new(File.join(File.dirname(__FILE__), 'db/craft-beer-book'))
 DB.results_as_hash = true
@@ -20,6 +22,19 @@ end
 
 get '/add' do
   erb :add
+end
+
+get '/searchresults' do
+  @query = params["query"]
+  @results = BAScraperResults.new(@query).search_title_scrape
+  erb :searchresults
+end
+
+get '/add_ba/:id' do
+  @url = params[:id]
+  @beer = BAScrapeProfile.new(@url).scrape
+
+  erb :add_ba
 end
 
 post '/madd' do
